9d2f9ffd9b9435d7fe9ac3c2a99187be21590cec
Justin <jvcruz@ucdavis.edu>
2018-03-21 21:46:12 -0700

    boxes added to ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div id="main-box"></div>
+    <div id="main-box">
+      <div id="mini-box">
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +12,7 @@
 export default {
   data () {
     return {
-
+      users: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
     }
   }
 }
@@ -23,5 +27,13 @@ export default {
   #main-box {
     background: rgba(255, 250, 181, 0.8);
     height: 90vh;
+    overflow: hidden;
+  }
+
+  #mini-box {
+    background: rgba(255, 248, 160, 0.6);
+    height: 700px;
+    width: 400px;
+    margin: 5% auto;
   }
 </style>
